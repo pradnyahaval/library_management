c50b737d47682ce506e1601d854fcde03eaecfb9
pradnyahaval <56785606+pradnyahaval@users.noreply.github.com>
2020-12-30 00:35:35 +0530

    Create insert_book.php

---
+++ source/librarian/insert_book.php
@@ -0,0 +1,68 @@
+<?php
+	require "../db_connect.php";
+	require "../message_display.php";
+	require "verify_librarian.php";
+	require "header_librarian.php";
+?>
+
+<html>
+	<head>
+		<title>Add book</title>
+		<link rel="stylesheet" type="text/css" href="../css/global_styles.css" />
+		<link rel="stylesheet" type="text/css" href="../css/styles_for_all.css">
+	</head>
+	<body>
+		<form class="cd-form" method="POST" action="#">
+			<div class="box">
+				<legend>Enter book details</legend>
+
+					<div class="error-message" id="error-message">
+						<p id="error"></p>
+					</div>
+
+					<input type="number" id="inputdefault" name="b_isbn" placeholder="ISBN" required />
+
+				</br>
+					<input type="text" id="inputdefault" name="b_title" placeholder="Title" required />
+				</br>
+					<input type="text" id="inputdefault" name="b_author" placeholder="Author" required />
+					</br>
+					<h4>Category</h4>
+						<p>
+							<select id="inputdefault" name="b_category">
+								<option>Fiction</option>
+								<option>Non-fiction</option>
+								<option>Education</option>
+							</select>
+						</p>
+					<input type="number" id="inputdefault" name="b_price" placeholder="Price" required />
+				</br>
+					<input type="number" id="inputdefault" name="b_copies" placeholder="Copies" required />
+				</br>
+					<input type="submit" id="button" name="b_add" value="Add book" />
+				</div>
+			</form>
+
+	<body>
+
+	<?php
+		if(isset($_POST['b_add']))
+		{
+			$query = $con->prepare("SELECT isbn FROM book WHERE isbn = ?;");
+			$query->bind_param("s", $_POST['b_isbn']);
+			$query->execute();
+
+			if(mysqli_num_rows($query->get_result()) != 0)
+				echo error_with_field("A book with that ISBN already exists", "b_isbn");
+			else
+			{
+				$query = $con->prepare("INSERT INTO book VALUES(?, ?, ?, ?, ?, ?);");
+				$query->bind_param("ssssdd", $_POST['b_isbn'], $_POST['b_title'], $_POST['b_author'], $_POST['b_category'], $_POST['b_price'], $_POST['b_copies']);
+
+				if(!$query->execute())
+					die(error_without_field("ERROR: Couldn't add book"));
+				echo success("Successfully added book");
+			}
+		}
+	?>
+</html>
